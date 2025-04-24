81b84671d988b2b3551d8a9c70a79aa7ff2014ec
beqaperanidze <beqaperanidze28@gmail.com>
2025-04-24 09:51:56 +0400

    initial commit

---
+++ ProductManagement/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductManagement.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
